2d972642842a74fa8b9a15824c8cde8c6e557018
Roamer15 <ianseeksbeta@gmail.com>
2025-03-06 10:16:31 +0100

    add contact form completed

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const InitialContact = {
+    name: "",
+    email: "",
+    telephone: "",
+  };
--- src/pages/NewContact.jsx
+++ src/pages/NewContact.jsx
@@ -1,11 +1,50 @@
-import { useFormik } from "formik";
-import { AddContactSchema } from "../schemas/contact-schema";
+// import { useFormik } from "formik";
+// import { AddContactSchema } from "../schemas/contact-schema";
+// import { InitialContact } from '../constants'
 
 export default function NewContact() {
+
+    // const Error = ({ error }) => {
+    //     return error? <div>
+    //         <div className="error-container">
+    //         <p className="form_error">{error}</p>
+    //         </div>
+    //         </div>: null;
+    // }
+
+    // const { values, handleBlur, handleChange, handleSubmit, errors } =
+    // useFormik({
+    //     InitialContact,
+    //   validationSchema: AddContactSchema,
+    // });
+
+
     return (
         <div className="form">
-            <h2>Create A a new contact</h2>
-            <form className="contact-form" onSubmit={handleSubmit}></form>
+            <h2>Create A New Contact</h2>
+            <form className="contact-form">
+            <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+          />
+            <label htmlFor="telephone">Number</label>
+          <input
+            type="tel"
+            name="telephone"
+             placeholder="Phone"
+          />
+
+            <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+             placeholder="Email"
+          />
+
+          <button type="submit">Add Contact</button>
+            </form>
         </div>
     )
 }
