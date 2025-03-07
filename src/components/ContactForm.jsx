fce3aeed28430df5cc6fb63f69a2a5717fff0537
Roamer15 <ianseeksbeta@gmail.com>
2025-03-07 12:51:20 +0100

    Mobile UI fixed

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,68 +1,125 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-export default function ContactForm({ formData, handleChange, handleFileChange, handleSubmit, errors, isEditing }) {
-    return (
-        <form className="contact-form" onSubmit={handleSubmit}>
-            <label htmlFor="name"></label>
-            <input
-                type="text"
-                name="name"
-                placeholder='Name'
-                value={formData.name}
-                onChange={handleChange}
-                required
-            />
-            {errors?.name && <p className="form_error">{errors.name}</p>}
+export default function ContactForm({
+  formData,
+  handleChange,
+  handleFileChange,
+  handleSubmit,
+  errors,
+  isEditing,
+}) {
+  return (
+    <form className="contact-form" onSubmit={handleSubmit}>
+      <div className="input-container">
+        <input
+          placeholder="Enter your name"
+          className="input-field"
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="name" className="input-label">
+          Name
+        </label>
+        <span className="input-highlight"></span>
+        
+      </div>
+      {errors?.name && <p className="form_error">{errors.name}</p>}
 
-            <label htmlFor="telephone"></label>
-            <input
-                type="tel"
-                name="telephone"
-                placeholder='Telephone'
-                value={formData.telephone}
-                onChange={handleChange}
-                required
-            />
-            {errors?.telephone && <p className="form_error">{errors.telephone}</p>}
+      <div className="input-container">
+        <input
+          placeholder="Enter your phone number"
+          className="input-field"
+          type="text"
+          name="telephone"
+          value={formData.telephone}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="telephone" className="input-label">
+          Telephone
+        </label>
+        <span className="input-highlight"></span>
+       
 
-            <label htmlFor="email"></label>
-            <input
-                type="email"
-                name="email"
-                placeholder='Email'
-                value={formData.email}
-                onChange={handleChange}
-                required
-            />
-            {errors?.email && <p className="form_error">{errors.email}</p>}
+      </div>
+      {errors?.telephone && <p className="form_error">{errors.telephone}</p>}
 
-            <label htmlFor="profilePic">Profile Picture</label>
-            <input
-                type="file"
-                name="profilePic"
-                accept="image/*"
-                onChange={handleFileChange}
-            />
-            {errors?.profilePic && <p className="form_error">{errors.profilePic}</p>}
+      <div className="input-container">
+        <input
+          placeholder="Enter your email"
+          className="input-field"
+          type="text"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="email" className="input-label">
+          Email
+        </label>
+        <span className="input-highlight"></span>
+       
+      </div>
+      {errors?.email && <p className="form_error">{errors.email}</p>}
+      <label className="custum-file-upload" htmlFor="file">
+        <div className="icon">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="" viewBox="0 0 24 24">
+            <g strokeWidth="0" id="SVGRepo_bgCarrier"></g>
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              id="SVGRepo_tracerCarrier"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              <path
+                fill=""
+                d="M10 1C9.73478 1 9.48043 1.10536 9.29289 1.29289L3.29289 7.29289C3.10536 7.48043 3 7.73478 3 8V20C3 21.6569 4.34315 23 6 23H7C7.55228 23 8 22.5523 8 22C8 21.4477 7.55228 21 7 21H6C5.44772 21 5 20.5523 5 20V9H10C10.5523 9 11 8.55228 11 8V3H18C18.5523 3 19 3.44772 19 4V9C19 9.55228 19.4477 10 20 10C20.5523 10 21 9.55228 21 9V4C21 2.34315 19.6569 1 18 1H10ZM9 7H6.41421L9 4.41421V7ZM14 15.5C14 14.1193 15.1193 13 16.5 13C17.8807 13 19 14.1193 19 15.5V16V17H20C21.1046 17 22 17.8954 22 19C22 20.1046 21.1046 21 20 21H13C11.8954 21 11 20.1046 11 19C11 17.8954 11.8954 17 13 17H14V16V15.5ZM16.5 11C14.142 11 12.2076 12.8136 12.0156 15.122C10.2825 15.5606 9 17.1305 9 19C9 21.2091 10.7909 23 13 23H20C22.2091 23 24 21.2091 24 19C24 17.1305 22.7175 15.5606 20.9844 15.122C20.7924 12.8136 18.858 11 16.5 11Z"
+                clipRule="evenodd"
+                fillRule="evenodd"
+              ></path>
+            </g>
+          </svg>
+        </div>
+        <div className="text">
+          <span>Click to upload image</span>
+        </div>
+        <input
+          id="file"
+          type="file"
+          name="profilePic"
+          accept="image/*"
+          onChange={handleFileChange}
+        />
+      </label>
+      {errors?.profilePic && <p className="form_error">{errors.profilePic}</p>}
 
-            {formData.profilePic && (
-                <img
-                    src={typeof formData.profilePic === 'string' ? formData.profilePic : URL.createObjectURL(formData.profilePic)}
-                    alt="Profile Preview"
-                    style={{ width: '100px', height: '100px', marginTop: '10px' }}
-                />
-            )}
+      {formData.profilePic && (
+        <img
+          src={
+            typeof formData.profilePic === "string"
+              ? formData.profilePic
+              : URL.createObjectURL(formData.profilePic)
+          }
+          alt="Profile Preview"
+          style={{ width: "100px", height: "100px", marginTop: "10px" }}
+        />
+      )}
 
-            <button type="submit">{isEditing ? 'Save Changes' : 'Add Contact'}</button>
-        </form>
-    );
+      <button type="submit">
+        {isEditing ? "Save Changes" : "Add Contact"}
+      </button>
+    </form>
+  );
 }
 
 ContactForm.propTypes = {
-    formData: PropTypes.object.isRequired,
-    handleChange: PropTypes.func.isRequired,
-    handleFileChange: PropTypes.func.isRequired,
-    handleSubmit: PropTypes.func.isRequired,
-    errors: PropTypes.object,
-    isEditing: PropTypes.bool
+  formData: PropTypes.object.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  handleFileChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  errors: PropTypes.object,
+  isEditing: PropTypes.bool,
 };
